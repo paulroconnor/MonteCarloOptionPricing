db08ee515b353509acb809fff997c43e4bf0bd97
paulroconnor <149101926+paulroconnor@users.noreply.github.com>
2024-07-23 21:37:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 // Main.cpp
 
-
 #include <iostream>
 #include "Option.h"
 
@@ -8,8 +7,8 @@ int main(int argc, const char * argv[])
 {
     Option option(100, 100, 0.05, 0.15, 5, 1);
 
-    std::cout << "Asset Price           : " << option.getAssetPrice() << std::endl
-              << "Strike Price          : " << option.getStrikePrice() << std::endl 
+    std::cout << "Asset Price           : $" << option.getAssetPrice() << std::endl
+              << "Strike Price          : $" << option.getStrikePrice() << std::endl 
               << "Growth Rate           : " << option.getGrowthRate() << std::endl
               << "Volatility            : " << option.getVolatility() << std::endl
               << "Years To Maturity     : " << option.getYearsToMaturity() << std::endl
