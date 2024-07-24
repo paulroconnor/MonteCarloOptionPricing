03902cf10342fa084ca23c19b04017c645a52ad6
paulroconnor <149101926+paulroconnor@users.noreply.github.com>
2024-07-24 15:58:19 -0400

    Add Price Path Generator

--- option.cpp
+++ option.cpp
@@ -1,8 +1,9 @@
-// option.cpp
+// Option.cpp
 
 #include "Option.h"
 #include <cmath>
 #include <random>
+#include <vector>
 
 Option::Option(
     float S,  // Asset Price
@@ -10,7 +11,7 @@ Option::Option(
     float r,  // Growth Rate
     float v,  // Volatility
     float Y,  // Years To Maturity
-    long N// Number of Simulations
+    long N    // Number of Simulations
 )
 {
     AssetPrice = S;
@@ -21,6 +22,32 @@ Option::Option(
     NumberOfSimulations = N;
 }
 
+// Generate Asset Price Path ---------------------------------------------
+
+std::vector<float>* Option::generateAssetPath()
+{
+    float timeStep = 1 / TRADING_DAYS_PER_YEAR;
+    float sqrtTimeStep = sqrt(timeStep);
+    int timeToMaturity = static_cast<int>(std::round(getYearsToMaturity() * TRADING_DAYS_PER_YEAR));  // round double to int
+
+    // Set up random normally distributed number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<float> d(0, 1);
+    double z;
+
+    // Create empty vector to store prices
+    std::vector<float>* pricePath = new std::vector<float>(timeToMaturity);
+    (*pricePath)[0] = static_cast<float>(getAssetPrice());
+
+    for (int t = 1; t <= timeToMaturity - 1; t++)
+    {
+        z = d(gen);
+        (*pricePath)[t] = (*pricePath)[t - 1] * exp(getGrowthRate() * timeStep + 0.5 * getVolatility() * sqrtTimeStep * z);
+    };
+    
+    return pricePath;
+}
 
 
 // Getting Variables -----------------------------------------------------
